e5fef8eaaaa88c72740db22226413b96848d929b
yugo <vnaticzhock@gmail.com>
2023-12-27 23:27:38 +0800

    silent hill

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ python3 ./main.py \
     --model_path alibaba-pai/pai-bloom-1b1-text2prompt-sd \
     --sd_lora_path ./lora_model \
     --output_path ./finetuned \
-    --sd_model_id runwayml/stable-diffusion-v1-5 \
+    --sd_model_id stabilityai/stable-diffusion-2 \
     --data_path ./movie_plot.csv \
-    --movie_title "The Terminator" \
+    --movie_title "Silent Hill" \
