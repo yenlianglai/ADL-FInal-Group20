77412f9bb24d173eb9268337ebfacda6113acd49
yugo <vnaticzhock@gmail.com>
2023-12-27 22:33:33 +0800

    first try

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 python3 ./main.py \
     --model_path alibaba-pai/pai-bloom-1b1-text2prompt-sd \
-    --output_path ./output \
+    --sd_lora_path ./lora_model \
+    --output_path ./finetuned \
     --sd_model_id runwayml/stable-diffusion-v1-5 \
     --data_path ./movie_plot.csv \
-    --movie_title "The Dark Knight" \
+    --movie_title "The Terminator" \
